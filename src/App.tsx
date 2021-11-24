ee9c5fa1d56b587675c46f17046e0d0f2a56705a
PathakPratik <rajpratik10@gmail.com>
2021-11-24 23:16:00 +0000

    added disable logic for buttons

--- src/App.tsx
+++ src/App.tsx
@@ -15,14 +15,19 @@ const useStyles = makeStyles({
 
 let watchID: number = 0; 
 
+const START_TRIP_BTN = 'startTrip'
+const STOP_TRIP_BTN = 'stopTrip'
+
 const App = () => {
 
   const classes = useStyles();
 
   const [loctionData, setLocationData] = React.useState([])
+  const [disabledBtn, setDisabledBtn] = React.useState(STOP_TRIP_BTN)
 
   const handleClickStartTracking = () => {
     watchID = startTracking() as number
+    setDisabledBtn(START_TRIP_BTN)
   }
 
   const handleClickStopTracking = () => {
@@ -30,12 +35,13 @@ const App = () => {
     const final = localStorage.getItem("locations") || "[]"
     setLocationData(JSON.parse(final))
     localStorage.removeItem("locations")
+    setDisabledBtn(STOP_TRIP_BTN)
   }
 
   return (
     <div className="App">
-      <Button variant="outlined" color="primary" className={classes.root} onClick={handleClickStartTracking} >Start Tracking</Button>
-      <Button variant="outlined" color="primary" className={classes.root} onClick={handleClickStopTracking} >Stop Tracking</Button>
+      <Button variant="outlined" color="primary" className={classes.root} onClick={handleClickStartTracking} disabled={disabledBtn === START_TRIP_BTN}>Start Tracking</Button>
+      <Button variant="outlined" color="primary" className={classes.root} onClick={handleClickStopTracking} disabled={disabledBtn === STOP_TRIP_BTN}>Stop Tracking</Button>
       <CSVLink data={loctionData}>Download Data</CSVLink>
     </div>
   );
