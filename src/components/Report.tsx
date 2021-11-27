0b0081d461b0ea3ccedececf454d1474495b83b1
PathakPratik <rajpratik10@gmail.com>
2021-11-27 16:31:28 +0000

    added report issue btn

--- src/components/Report.tsx
+++ src/components/Report.tsx
@@ -1,9 +1,19 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
+import { Button } from "@material-ui/core";
 
 const Report = () => {
 
+  const handleImageChange = () => {
+    alert("success")
+  }
+
   return (
-    <div>Test</div>
+    <div>
+        <label>
+        <input style={{display: 'none'}} type="file" accept="image/*" capture="environment" onChange={handleImageChange}></input>
+        <Button variant="contained" style={{marginTop: '4rem'}} color="primary" >Report Issue</Button>
+        </label>
+    </div>
   );
 }
  
